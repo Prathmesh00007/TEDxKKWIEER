f07638560c95a88806bc68eb6cc436284545204b
Prathmesh00007 <prathmeshranade23@gmail.com>
2025-03-24 23:43:04 +0530

    h

--- static/src/components/sponsors.jsx
+++ static/src/components/sponsors.jsx
@@ -108,17 +108,18 @@ const styles = {
 const Sponsors = () => {
   return (
     <>
-      <section className="about-fixed-image fixed-image-heading mt-5">
-        <div>
-          <h1 className="my-4 text-center" style={{ fontFamily: "'Poppins', sans-serif" }}>
-            <span style={{ color: "red" }}>TEDx</span> KKWIEER
-          </h1>
-          <h2 className="my-3 text-center blur-fixed-image" style={{ fontFamily: "'Poppins', sans-serif" }}>
-            Special thanks to our valued sponsors for their generous support and contributions
-          </h2>
-          <Sponsor />
-        </div>
-      </section>
+      <section className="about-fixed-image h-auto fixed-image-heading mt-5">
+  <div>
+    <h1 className="my-4 text-center" style={{ fontFamily: "'Poppins', sans-serif" }}>
+      <span style={{ color: "red" }}>TEDx</span> KKWIEER
+    </h1>
+    <h2 className="my-3 text-center blur-fixed-image" style={{ fontFamily: "'Poppins', sans-serif" }}>
+      Special thanks to our valued sponsors for their generous support and contributions
+    </h2>
+    <Sponsor />
+  </div>
+</section>
+
     </>
   );
 };
